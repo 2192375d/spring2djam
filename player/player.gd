519a1b128a00b6058194097adf3bc66c040b3241
2192375d <173829746yzpq@gmail.com>
2026-04-24 15:20:34 -0400

    feat: ui done, connected to player stats

--- player/player.gd
+++ player/player.gd
@@ -1,5 +1,21 @@
 extends CharacterBody2D
+
 @export var speed: float
+
+var current_hunger: float
+var full_hunger: float
+
+func upgrade(new_full_hunger: float) -> void:
+	 #change the type of animal to something new
+	full_hunger = new_full_hunger
+	
+	GameSession.player_hunger_bar.max_value = full_hunger
+	current_hunger = full_hunger
+	GameSession.player_hunger_bar.value = current_hunger
+
+func _ready() -> void:
+	
+	upgrade(300)
 
 func _physics_process(_delta: float) -> void:
 	
