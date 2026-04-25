9bb9db253c33696b3c96db46ea7bb7a855abb8e2
2192375d <173829746yzpq@gmail.com>
2026-04-24 23:47:16 -0400

    feat: death animation

--- world/world.gd
+++ world/world.gd
@@ -4,8 +4,3 @@ class_name World
 
 func _ready() -> void:
 	GameSession.world = self
-	GameSession.change_playing_animal(Constants.EntityID.KIWI)
-
-
-func _on_hunger_drain_timer_timeout() -> void:
-	GameSession.player_drain_hunger()
