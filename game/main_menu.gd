e343c794af01b30f14dc91d26b284ca26be08d76
MotallebiMaria <motallebimaria@gmail.com>
2026-04-25 23:24:52 -0400

    add start menu

---
+++ game/main_menu.gd
@@ -0,0 +1,26 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_btn_play_pressed() -> void:
+	print("Play pressed")
+	get_tree().change_scene_to_file("res://game/world.tscn")
+
+
+func _on_btn_options_pressed() -> void:
+	print("Options pressed")
+	# the rest is to be implemented once pause/settings menu is made
+
+
+func _on_btn_exit_pressed() -> void:
+	print("Exit pressed")
+	get_tree().quit()
