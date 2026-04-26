4ea60ade1b70494cc248556df1b97c8cf3cf625a
yoshixi <davidchenyuhe@proton.me>
2026-04-26 10:10:33 -0400

    fix: removing unsued

--- game/world.gd
+++ game/world.gd
@@ -16,17 +16,6 @@ var tier3_enums : Array[int]
 var tier4_enums : Array[int]
 var dominant_enums : Array[int]
 
-@export var plant_timer : Timer
-@export var tier0_timer : Timer
-@export var tier1_timer : Timer
-@export var tier2_timer : Timer
-@export var tier3_timer : Timer
-@export var tier4_timer : Timer
-@export var dominant_timer : Timer
-
-@export var low_spawn : Area2D
-@export var med_spawn : Area2D
-@export var high_spawn : Area2D
 
 @onready var animals_root: Node2D = $animals
 @onready var player: Player = $Player
@@ -34,75 +23,13 @@ var dominant_enums : Array[int]
 func _ready() -> void:
 	AudioManager.play_game_music()
 	GameSession.world = self
-	for key in Constants.EntityID:
-		var entityid = Constants.EntityID[key]
-		if (entityid < 0): 
-			plants_enums.push_back(entityid)
-		elif (0 <= entityid && entityid < 100): 
-			tier0_enums.push_back(entityid)
-		elif (100 <= entityid  && entityid < 200): 
-			tier1_enums.push_back(entityid)
-		elif (200 <= entityid  && entityid < 300): 
-			tier2_enums.push_back(entityid)
-		elif (300 <= entityid && entityid < 400): 
-			tier3_enums.push_back(entityid)
-		elif (400 <= entityid && entityid < 500): 
-			tier4_enums.push_back(entityid)
-		elif (500 <= entityid): 
-			dominant_enums.push_back(entityid)
-	_connect_legacy_spawn_timer(plant_timer, plants_enums)
-	_connect_legacy_spawn_timer(tier0_timer, tier0_enums)
-	_connect_legacy_spawn_timer(tier1_timer, tier1_enums)
-	_connect_legacy_spawn_timer(tier2_timer, tier2_enums)
-	_connect_legacy_spawn_timer(tier3_timer, tier3_enums)
-	_connect_legacy_spawn_timer(tier4_timer, tier4_enums)
-	_connect_legacy_spawn_timer(dominant_timer, dominant_enums)
+
 
 func _process(_delta: float) -> void:
 	_update_relation_lights()
 
-func _connect_legacy_spawn_timer(timer: Timer, targetlist: Array[int]) -> void:
-	if timer == null:
-		return
-	if !timer.timeout.is_connected(timeout_spawn.bind(targetlist)):
-		timer.timeout.connect(timeout_spawn.bind(targetlist))
-	
-func random_position(spawn_enum : int) -> Vector2:
-	var ranges : Array
-	if low_spawn and (spawn_enum & Constants.SpawnHeight.LOW):
-		ranges.append(low_spawn)
-	if med_spawn and (spawn_enum & Constants.SpawnHeight.MEDIUM):
-		ranges.append(med_spawn)
-	if high_spawn and (spawn_enum & Constants.SpawnHeight.HIGH):
-		ranges.append(high_spawn)
-	if (len(ranges) != 0):
-		var target_area : Area2D = ranges[randi_range(0, len(ranges)-1)]
-		var rect = target_area.get_node("CollisionShape2D").shape.get_rect()
-		var top_left = rect.position
-		var bottom_right = rect.end
-		
-		var debug = Vector2( randi_range(top_left.x, bottom_right.x), randi_range(top_left.y, bottom_right.y) )
-		return debug
-	
-	return Vector2(0,0) # NEVER LET THIS FUCKING HAPPEN, PUT IN YOUR AREAS
-
-func spawn_entity(entityenum : int):
-	return
-	var animal_scene: PackedScene = Constants.entity_dict.get(entityenum)
-	if (animal_scene):
-		var animal : Animal = animal_scene.instantiate()
-		animals_root.add_child(animal)
-		# get animal spawn position enums
-		var pos : Vector2 = random_position(animal.entity_resource.spawn_height)
-		animal.global_position = pos
-		animal.position = pos
-		animal.domain_point = pos
 
 
-func timeout_spawn(targetlist : Array) -> void:
-	if (targetlist):
-		var randval = targetlist[randi_range(0, len(targetlist)-1)]
-		spawn_entity(randval)
 
 func _update_relation_lights() -> void:
 	if !player or !player.animal or !is_instance_valid(player.animal):
