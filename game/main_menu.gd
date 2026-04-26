91ab55fbce71759c29aa834b38d3d4d66ba4450e
MotallebiMaria <motallebimaria@gmail.com>
2026-04-26 12:08:51 -0400

    options menu

--- game/main_menu.gd
+++ game/main_menu.gd
@@ -1,9 +1,12 @@
 extends Control
 
+@onready var label = $Panel/Label
+@onready var button_box = $Panel/VBoxContainer
+@onready var options_menu = $OptionsMenu
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	AudioManager.play_ambient()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,6 +15,7 @@ func _process(delta: float) -> void:
 
 
 func _on_btn_play_pressed() -> void:
+	AudioManager.stop_ambient()
 	AudioManager.play_ui_click()
 	print("Play pressed")
 	get_tree().change_scene_to_file("res://game/world.tscn")
@@ -20,8 +24,11 @@ func _on_btn_play_pressed() -> void:
 func _on_btn_options_pressed() -> void:
 	AudioManager.play_ui_click()
 	print("Options pressed")
-	# the rest is to be implemented once pause/settings menu is made
-	pass
+	
+	label.hide()
+	button_box.hide()
+	
+	options_menu.open_options("main_menu")
 
 func _on_btn_exit_pressed() -> void:
 	AudioManager.play_ui_click()
